f7fc54ebe66e7b8670e92372a7456c4fa7bbb6e2
Artur Morys - Magiera <artus9033@gmail.com>
2020-02-14 11:34:10 +0100

    Prettified

--- temat8/potega.c
+++ temat8/potega.c
@@ -3,15 +3,18 @@
 
 double power(double x, int n)
 {
-    double w=1;
-    for(int i=0;i<n;i++)
-        w=w*x;
+    double w = 1;
+    
+    for(int i = 0; i < n; i++)
+        w = w * x;
+    
     return w;
 }
 
 int main()
 {
-    printf("%lf \n",power(3,2));
-    printf("%lf",power(2,4));
+    printf("%lf \n", power(3, 2));
+    printf("%lf", power(2, 4));
+    
     return 0;
 }
