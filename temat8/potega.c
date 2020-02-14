944ee09495c1bfbe403c0acff66c3beaf0010eb4
eSqadron <61045292+eSqadron@users.noreply.github.com>
2020-02-14 10:14:46 +0100

    Create potega.c

---
+++ temat8/potega.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double power(double x, int n)
+{
+    double w=1;
+    for(int i=0;i<n;i++)
+        w=w*x;
+    return w;
+}
+
+int main()
+{
+    printf("%lf \n",power(3,2));
+    printf("%lf",power(2,4));
+    return 0;
+}
