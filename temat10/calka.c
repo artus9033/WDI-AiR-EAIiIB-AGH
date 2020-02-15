f04ce20403363825e49f7d749d399f4d4a35383b
Artur Morys - Magiera <artus9033@gmail.com>
2020-02-15 18:08:23 +0100

    Prettified

--- temat10/calka.c
+++ temat10/calka.c
@@ -2,20 +2,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
 double integral(double (*f)(double), double x1, double x2, double prec)
 {
-    double w=0;
-    for(double i=x1;i<x2;i+=prec)
-        w+=((*f)(i+prec/2))*prec;
+    double w = 0;
+    for(double i = x1; i < x2; i += prec)
+        w += ((*f) (i + prec / 2)) * prec;
     return w;
 }
+
 double y(double x)
 {
     return x;
 }
+
 int main()
 {
-    printf("%lf\n",integral(sin,0,M_PI,0.001));
-    printf("%lf\n",integral(y,0,M_PI,0.001));
+    printf("%lf \n", integral(sin, 0, M_PI, 0.001));
+    printf("%lf \n", integral(y, 0, M_PI, 0.001));
+    
     return 0;
 }
